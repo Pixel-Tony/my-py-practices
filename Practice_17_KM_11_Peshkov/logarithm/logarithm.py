2ef663a3e942a655a4ea6a519c3702eca61b5181
Pixel-Tony <ant.peshkoff9@gmail.com>
2021-12-23 22:32:41 +0200

    Typos fixed and logarithm is now my own secret development

--- Practice_17_KM_11_Peshkov/logarithm/logarithm.py
+++ Practice_17_KM_11_Peshkov/logarithm/logarithm.py
@@ -1,17 +1,35 @@
 import math as m
 
 def log(a, b):
-    '''Logarythm of a to base b'''
+    '''Logarithm of a to base b'''
     if a < 0 or b < 0:
         raise ValueError('Expected positive, got negative')
-    return m.log(a, b)
+    if b == 1:
+        raise ValueError('Base must not equal 1')
+    def _(a, b): # written by myself
+        res = 0
+        counter = 0
+        if a < b:
+            return -_(b, a)
+        if b == 1:
+            return a
+        if b < 1:
+            return -_(a, 1/b)
+        while a >= b:
+            a /= b
+            counter += 1
+        res += counter
+        if a == 1:
+            return res
+        return res + 1/_(b, a)
+    return _(a, b)
 
 def ln(a):
-    '''Natural logarythm of a'''
+    '''Natural logarithm of a'''
     return log(a, m.e)
 
 def lg(a):
-    '''Base 10 logarythm of a'''
+    '''Base 10 logarithm of a'''
     return log(a, 10)
 
 __all__ = ['log', 'ln', 'lg']
